ad4d5f4c4b1a65550034d7163e675a1da2cf9b46
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-03-28 06:20:19 +0300

    Update speechRecognition.py

--- speechRecognition.py
+++ speechRecognition.py
@@ -1,12 +1,35 @@
 import speech_recognition as sr
+import tkinter as tk
+from tkinter import filedialog
 
+# create a new SpeechRecognition instance
 r = sr.Recognizer()
 
-# load the audio file
-with sr.AudioFile("path/to/audio/file.wav") as source:
-    audio_data = r.record(source)
+# create a GUI window using tkinter
+window = tk.Tk()
 
-# use Google Speech Recognition to convert the audio to text
-text = r.recognize_google(audio_data)
+# define a function to handle the drag and drop event
+def handle_drop(event):
+    # get the file path from the dropped event
+    file_path = event.data['text'].replace('file:///', '/')
 
-print(text)
+    # load the audio file
+    with sr.AudioFile(file_path) as source:
+        # read the audio data from the file
+        audio_data = r.record(source)
+
+    # use Google Speech Recognition to convert the audio to text
+    text = r.recognize_google(audio_data)
+
+    # print the text
+    print(text)
+
+# create a label in the GUI window to indicate where to drop the file
+label = tk.Label(window, text="Drag and drop an audio file here")
+label.pack(pady=20)
+
+# bind the handle_drop function to the drag and drop event
+window.bind('<Drop>', handle_drop)
+
+# run the GUI window
+window.mainloop()
